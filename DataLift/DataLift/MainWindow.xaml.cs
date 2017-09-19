d22147a9c9c55186d424254efcda8e2ca81dee42
mcgeer <Rileypatrickmcgee@gmail.com>
2017-09-19 15:21:51 -0400

    Initial DataLift Framework/boilerplate plus Login UI

---
+++ DataLift/DataLift/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using DataLift.Nav;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataLift
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Remove junk page management
+            PageManager.window = this;
+            PageManager.SwitchPage(UIPages.LoginView);
+        }
+
+        internal void Navigate(UIPages nextPage)
+        {
+            Content = nextPage.page;
+        }
+    }
+}
