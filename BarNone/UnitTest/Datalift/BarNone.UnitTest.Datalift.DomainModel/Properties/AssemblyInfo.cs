c381e092a7bf1e9172c9acbe35149fb257a065b2
Christopher Stokes <c.stokes99@gmail.com>
2017-10-29 15:43:26 -0400

    add test project

---
+++ BarNone/UnitTest/Datalift/BarNone.UnitTest.Datalift.DomainModel/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BarNone.UnitTest.Datalift.DomainModel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BarNone.UnitTest.Datalift.DomainModel")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d11ed2b-ad84-4aa6-9438-8f69d8e03397")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
