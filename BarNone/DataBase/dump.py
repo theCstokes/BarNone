ba12f2dc045555ed49fb6593420f2cc3db02e11d
vgulatee <vishesh_g@hotmail.com>
2017-10-29 22:05:56 -0400

    Added dump and restore python scripts for db

---
+++ BarNone/DataBase/dump.py
@@ -0,0 +1,20 @@
+#!python
+
+import subprocess
+import os
+
+user = "postgres"
+pw = "admin"
+host = "127.0.0.1"
+db="SharpSight"
+ 
+dir_name = os.getcwd()
+dump = """ "c:\\program files\\postgresql\\10\\bin\\pg_dump" -U %s -Z 9 -f %s -F c %s  """                  
+os.putenv('PGPASSWORD', pw)
+ 
+file_name = db  + ".backup"
+command = dump % (user,  dir_name +"\\"+ file_name, db)
+print(command)
+subprocess.call(command,shell = True)
+print("%s Dump finished" % db)
+
---
+++ BarNone/DataBase/restore.py
@@ -0,0 +1,20 @@
+#!python
+
+import subprocess
+import os
+
+user = "postgres"
+pw = "admin"
+host = "127.0.0.1"
+db="SharpSight"
+
+dir_name = os.getcwd()
+restore = """ "c:\\program files\\postgresql\\10\\bin\\pg_restore" -U %s -d %s %s """   #Change number based on whatever version you are using                
+os.putenv('PGPASSWORD', pw)
+
+file_name = db + ".backup"   
+command = restore % (user, db, dir_name + "\\"+ file_name)
+print(command)
+subprocess.call(command,shell = True)
+print("%s Restore finished" % db)
+
