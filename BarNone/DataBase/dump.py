55971a9e5168ace4d1e746927062f7e2197fbbd2
Christopher Stokes <c.stokes99@gmail.com>
2017-10-31 08:59:47 -0400

    update db scripts

--- BarNone/DataBase/dump.py
+++ BarNone/DataBase/dump.py
@@ -6,14 +6,15 @@ import os
 user = "postgres"
 pw = "admin"
 host = "127.0.0.1"
+port = "5433";
 db="BarNone"
  
 dir_name = os.getcwd()
-dump = """ "c:\\program files\\postgresql\\10\\bin\\pg_dump" -U %s -Z 9 -f "%s" -F c %s  """                  
+dump = """ "c:\\program files\\postgresql\\10\\bin\\pg_dump" -h %s -p %s -U %s -Z 9 -f "%s" -F c %s  """                  
 os.putenv('PGPASSWORD', pw)
  
 file_name = db  + ".backup"
-command = dump % (user,  dir_name +"\\"+ file_name, db)
+command = dump % (host, port, user,  dir_name +"\\"+ file_name, db)
 print(command)
 subprocess.call(command,shell = True)
 print("%s Dump finished" % db)
--- BarNone/DataBase/restore.py
+++ BarNone/DataBase/restore.py
@@ -6,14 +6,15 @@ import os
 user = "postgres"
 pw = "admin"
 host = "127.0.0.1"
-db="SharpSight"
+port = "5433";
+db="BarNone"
 
 dir_name = os.getcwd()
-restore = """ "c:\\program files\\postgresql\\10\\bin\\pg_restore" --clean -U %s -d %s "%s" """   #Change number based on whatever version you are using                
+restore = """ "c:\\program files\\postgresql\\10\\bin\\pg_restore" -h %s -p %s --clean -U %s -d %s "%s" """   #Change number based on whatever version you are using                
 os.putenv('PGPASSWORD', pw)
 
 file_name = db + ".backup"   
-command = restore % (user, db, dir_name + "\\"+ file_name)
+command = restore % (host, port, user, db, dir_name + "\\"+ file_name)
 print(command)
 subprocess.call(command,shell = True)
 print("%s Restore finished" % db)
