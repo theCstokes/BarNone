cbc25e05c6f8273abcaa0f47fe14baf7ec46e167
Riley McGee <mcgeer@mcmaster.ca>
2017-11-23 19:07:23 -0500

    Login and Register are Material

--- BarNone/DataLift/BarNone.DataLift.Application/ViewModels/RegisterScreenVM.cs
+++ BarNone/DataLift/BarNone.DataLift.Application/ViewModels/RegisterScreenVM.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using BarNone.DataLift.UI.Commands;
+using BarNone.DataLift.UI.Nav;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BarNone.DataLift.UI.ViewModels
 {
@@ -10,9 +13,9 @@ namespace BarNone.DataLift.UI.ViewModels
     {
         public string Username { get; set; }
 
-        public void OnUsernameChanged()
-        {
+        public ICommand BackCommand { get; } = new RelayCommand(action => PageManager.SwitchPage(UIPages.LoginView));
 
-        }
+        public ICommand RegisterCommand { get; } = new RelayCommand(action => System.Diagnostics.Debug.WriteLine("Not Implemented Register Command!"));
+
     }
 }
