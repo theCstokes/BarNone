e76de539fda4c6f30a05969b584287c40521b033
Aamir Mansoor <mansoa4@mcmaster.ca>
2018-01-16 16:48:41 -0500

    adding code to implement lift list view

---
+++ BarNone/DataLift/BarNone.DataLift.Application/ViewModels/LiftListVM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarNone.DataLift.UI.ViewModels
+{
+    class LiftListVM : ViewModelBase
+    {
+        private string _liftName;
+        public string LiftName
+        {
+            get { return _liftName; }
+
+            set
+            {
+                if (_liftName == value) return;
+
+                _liftName = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("LiftName"));
+            }
+        }
+
+        private string _liftType;
+        public string LiftType
+        {
+            get { return _liftType; }
+
+            set
+            {
+                if (_liftType == value) return;
+
+                _liftType = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("LiftType"));
+            }
+        }
+    }
+}
