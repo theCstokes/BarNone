11f852258ed8b90e956f5cc7a31c9d893db07e20
Riley McGee <mcgeer@mcmaster.ca>
2018-01-16 17:02:54 -0500

    Added Singleton common class

---
+++ BarNone/DataLift/BarNone.DataLift.Application/ViewModels/Common/CurrentLiftDataVM.cs
@@ -0,0 +1,55 @@
+﻿using BarNone.Shared.DataTransfer;
+using BarNone.DataLift.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarNone.DataLift.UI.ViewModels.Common
+{
+    public class CurrentLiftDataVM : ViewModelBase
+    {
+        //If we need to get item modified ask me -Riley
+        private ObservableCollection<LiftDTO> _currentRecordedData = new ObservableCollection<LiftDTO>();
+        public ObservableCollection<LiftDTO> CurrentRecordedData
+        {
+            get => _currentRecordedData;
+            set
+            {
+                _currentRecordedData = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("CurrentRecordedData"));
+            }
+        }
+    }
+
+    #region Singleton Class
+    /// <summary>
+    /// Creates a singleton view model for shared lift information to prevent obsurd bindings
+    /// </summary>
+    public static class CurrentLiftDataVMSingleton
+    {
+        /// <summary>
+        /// CurrentLiftDataVM singleton
+        /// </summary>
+        private static CurrentLiftDataVM vm;
+
+        /// <summary>
+        /// Gets the singleton instance of CurrentLiftDataVM
+        /// </summary>
+        /// <returns>Singleton instance of CurrentLiftDataVM</returns>
+        public static CurrentLiftDataVM GetInstance()
+        {
+            if (vm == null)
+            {
+                vm = new CurrentLiftDataVM();
+            }
+            return vm;
+        }
+
+    }
+    #endregion
+
+}
