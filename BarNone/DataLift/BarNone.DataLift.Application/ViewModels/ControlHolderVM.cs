85a3d788fee8572c3fb346b1aa402c4af8b9f6d6
Aamir Mansoor <mansoa4@mcmaster.ca>
2018-01-18 14:09:26 -0500

    Commented ControlHolderVM

--- BarNone/DataLift/BarNone.DataLift.Application/ViewModels/ControlHolderVM.cs
+++ BarNone/DataLift/BarNone.DataLift.Application/ViewModels/ControlHolderVM.cs
@@ -17,13 +17,20 @@ namespace BarNone.DataLift.UI.ViewModels
     public class ControlHolderVM : ViewModelBase
     {
         #region Public Commands
+        /// <summary>
+        /// Asynchronous function that controls the warning dialog.
+        /// Shows warning telling the user that they may loose data if they go backwards in the workflow.
+        /// </summary>
+        /// <param name="o"></param>
         private async void ExecuteRunDialog(object o)
         {
+            // Create a new dialog.
             var view = new YesNoDialogScreen();
 
-            //let's set up a little MVVM, cos that's what the cool kids are doing:
+            //  Look for RootDialog in the XAML (view) and wait until the dialog has completed execution.
             var result = await DialogHost.Show(view, "RootDialog");
 
+            // If the user wishes to take the risk and potentially loose data move backwards in the workflow.
             if (Equals(result, false))
             {
                 MoveWorkflowBackward();
@@ -31,6 +38,9 @@ namespace BarNone.DataLift.UI.ViewModels
         }
         public ICommand RunDialogCommand => new RelayCommand(ExecuteRunDialog);
 
+        /// <summary>
+        /// Currently unused function.  Still bound to a button in the UI.  May be used in the future for debug.
+        /// </summary>
         public RelayCommand _TestStrategy1 { get; private set; }
         public ICommand TestStrategy1
         {
@@ -60,22 +70,7 @@ namespace BarNone.DataLift.UI.ViewModels
             }
         }
 
-        /// <summary>
-        /// Bound command to move wordlow backwards in XAML.
-        /// </summary>
-        public RelayCommand _MoveWorflowBackwardCmd { get; private set; }
-        public ICommand MoveWorflowBackwardCmd
-        {
-            get
-            {
-                if (_MoveWorflowBackwardCmd == null)
-                {
-                    _MoveWorflowBackwardCmd = new RelayCommand(action => MoveWorkflowBackward());
-                }
-                return _MoveWorflowBackwardCmd;
-            }
-        }
-
+        // Logout command.  Dumps user data and moves back to the login page.
         public ICommand LogoutCommand { get; } = new RelayCommand(action => PageManager.SwitchPage(UIPages.LoginView));
 
         /// <summary>
@@ -123,6 +118,10 @@ namespace BarNone.DataLift.UI.ViewModels
             }
         }
 
+        /// <summary>
+        /// Controls whether the move backwards in workflow button can be pressed.  
+        /// Because you cannot move backwards in the worflow if you are in the first step.
+        /// </summary>
         private bool _isBackwardsEnabled = false;
 
         public bool IsBackwardsEnabled
@@ -135,6 +134,9 @@ namespace BarNone.DataLift.UI.ViewModels
             }
         }
 
+        /// <summary>
+        /// Determines the opacity of the button for step 2 of the workflow.  0.5 is 50% opacity.
+        /// </summary>
         private double _isStepTwoEnabledController = 0.5;
         public double IsStepTwoEnabledController
         {
@@ -145,7 +147,10 @@ namespace BarNone.DataLift.UI.ViewModels
                 OnPropertyChanged(new PropertyChangedEventArgs("IsStepTwoEnabledController"));
             }
         }
-        
+
+        /// <summary>
+        /// Determines the opacity of the button for step 3 of the workflow.  0.5 is 50% opacity.
+        /// </summary>
         private double _isStepThreeEnabledController = 0.5;
         public double IsStepThreeEnabledController
         {
@@ -158,6 +163,10 @@ namespace BarNone.DataLift.UI.ViewModels
         }
 
         //TODO story board
+
+        /// <summary>
+        /// Dictates how far the progress bar is between step 1 and 2.  0 is 0% and 100 is 100%
+        /// </summary>
         private int _stepTwoProgressController = 0;
         public int StepTwoProgressController
         {
@@ -169,6 +178,10 @@ namespace BarNone.DataLift.UI.ViewModels
             }
         }
 
+
+        /// <summary>
+        /// Dictates how far the progress bar is between step 2 and 3.  0 is 0% and 100 is 100%
+        /// </summary>
         private int _stepThreeProgressController = 0;
         public int StepThreeProgressController
         {
@@ -184,20 +197,25 @@ namespace BarNone.DataLift.UI.ViewModels
 
         #region Private Variables
 
+        /// <summary>
+        /// private variable used to track waht state of the workflow the system is in.
+        /// </summary>
         private State _currentState = State.Recording;
-
-        private MessageBoxResult result;
 
         #endregion
 
         #region Private Enums
         /// <summary>
-        /// Enumeration defininng which state DataLift is in.
+        /// Private enumeration defininng which state DataLift is in.
         /// </summary>
         private enum State { Recording, Editing, Saving };
         #endregion
 
         #region Private Functions
+
+        /// <summary>
+        /// Currently unused function.  Bound to a button in the UI.  May be used in the future.
+        /// </summary>
         private void TestStrategy1_ResetKinectSensor()
         {
             //IsVisible = !IsVisible;
@@ -205,6 +223,7 @@ namespace BarNone.DataLift.UI.ViewModels
             //kinectSensor.Close();
             //kinectSensor.Open();
         }
+
         /// <summary>
         /// Moves forward in the Data Lift Workflow.  Changes state to move the workflow along.
         /// </summary>
@@ -248,15 +267,20 @@ namespace BarNone.DataLift.UI.ViewModels
         /// </summary>
         private void GotoRecordingState()
         {
+            // Change state to recording.
             _currentState = State.Recording;
 
+            // Make sure the user cannot go further back in the workflow.
             IsBackwardsEnabled = false;
             
+            // Make sure that the two buttons in the workflow have low opacity.
             IsStepTwoEnabledController = 0.5;
             IsStepThreeEnabledController = 0.5;
 
+            // Edit the progress bar accordingly.
             StepTwoProgressController = 0;
             
+            // Display the correct screen in the XAML.
             IsRecorderVisible = true;
             IsEditorVisible = false;
             IsSavingVisible = false;
@@ -267,16 +291,21 @@ namespace BarNone.DataLift.UI.ViewModels
         /// </summary>
         private void GotoEditingState()
         {
+            // Change state to editing.
             _currentState = State.Editing;
 
+            // Make sure the user can go backwards in the worflow.
             IsBackwardsEnabled = true;
 
+            // Set the correct opacities for the workflow view.
             IsStepTwoEnabledController = 1;
             IsStepThreeEnabledController = 0.5;
 
+            // Set the correct value for the progress bars.
             StepTwoProgressController = 100;
             StepThreeProgressController = 0;
 
+            // Display the correct screen in the XAML.
             IsRecorderVisible = false;
             IsEditorVisible = true;
             IsSavingVisible = false;
@@ -287,13 +316,17 @@ namespace BarNone.DataLift.UI.ViewModels
         /// </summary>
         private void GotoSavingState()
         {
+            // Change state to saving.
             _currentState = State.Saving;
-            
+
+            // Set the correct value for the progress bars.
             StepThreeProgressController = 100;
 
+            // Set the correct opacities for the workflow view.
             IsStepTwoEnabledController = 1;
             IsStepThreeEnabledController = 1;
-            
+
+            // Display the correct screen in the XAML.
             IsRecorderVisible = false;
             IsEditorVisible = false;
             IsSavingVisible = true;
