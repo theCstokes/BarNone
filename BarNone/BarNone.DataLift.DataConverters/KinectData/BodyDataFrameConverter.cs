221df70237300dc3da39061abadf5c75881259cb
Riley McGee <mcgeer@mcmaster.ca>
2018-01-19 18:34:17 -0500

    DL.DC commented

--- BarNone/BarNone.DataLift.DataConverters/KinectData/BodyDataFrameConverter.cs
+++ BarNone/BarNone.DataLift.DataConverters/KinectData/BodyDataFrameConverter.cs
@@ -8,12 +8,20 @@ using System.Linq;
 
 namespace BarNone.DataLift.DataConverters.KinectData
 {
+    /// <summary>
+    /// Body data Frame DM and DTO transforms
+    /// </summary>
     public class BodyDataFrameConverter : BaseDetailDataConverter<BodyDataFrame, BodyDataFrameDTO, BodyDataFrameDetailDTO, Converters>
     {
         public BodyDataFrameConverter(Converters converterContext, IDomainContext context) : base(converterContext, context)
         {
         }
 
+        /// <summary>
+        /// Translates dto to a BodyDataFrame
+        /// </summary>
+        /// <param name="dto">DTO being transformed</param>
+        /// <returns>dto as a BodyDataFrame</returns>
         public override BodyDataFrame OnCreateDataModel(BodyDataFrameDTO dto)
         {
             return new BodyDataFrame
@@ -23,6 +31,11 @@ namespace BarNone.DataLift.DataConverters.KinectData
             };
         }
 
+        /// <summary>
+        /// Fills in data with dto info
+        /// </summary>
+        /// <param name="data">DM to populate</param>
+        /// <param name="dto">DTO being transformed</param>
         public override void OnCreateDetailDataModel(BodyDataFrame data, BodyDataFrameDetailDTO dto)
         {
             data.Joints = dto.Joints?.Select(jointDTO => new Joint()
@@ -39,6 +52,11 @@ namespace BarNone.DataLift.DataConverters.KinectData
                 .ToDictionary(x => x.JointType, x => x);
         }
 
+        /// <summary>
+        /// Transforms data into its detail DTOs
+        /// </summary>
+        /// <param name="data">DM to transform</param>
+        /// <returns>DTO Details representation of data</returns>
         public override BodyDataFrameDetailDTO OnCreateDetailDTO(BodyDataFrame data)
         {
             return new BodyDataFrameDetailDTO
@@ -54,6 +72,11 @@ namespace BarNone.DataLift.DataConverters.KinectData
             };
         }
 
+        /// <summary>
+        /// Transforms data into its DTO form
+        /// </summary>
+        /// <param name="data">DM to transform</param>
+        /// <returns>DTO representation of data</returns>
         public override BodyDataFrameDTO OnCreateDTO(BodyDataFrame data)
         {
             return new BodyDataFrameDTO
