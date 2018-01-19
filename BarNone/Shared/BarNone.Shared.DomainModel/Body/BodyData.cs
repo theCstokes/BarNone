88410e36d195ea5c2d5651f6c26c59feab1aebf2
Chris Stokes <christopher.stokes@snowjob.ca>
2018-01-19 15:10:22 -0500

    assem

--- BarNone/Shared/BarNone.Shared.DomainModel/Body/BodyData.cs
+++ BarNone/Shared/BarNone.Shared.DomainModel/Body/BodyData.cs
@@ -1,6 +1,8 @@
 ﻿using BarNone.Shared.DomainModel.Core;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 
 namespace BarNone.Shared.DomainModel
