364c7fb066c0aca19c05496b08f0fe0c0fbfade7
Riley McGee <mcgeer@mcmaster.ca>
2018-01-18 20:03:10 -0500

    Commented Login Screen

--- BarNone/DataLift/BarNone.DataLift.Application/ViewModels/LoginScreenVM.cs
+++ BarNone/DataLift/BarNone.DataLift.Application/ViewModels/LoginScreenVM.cs
@@ -8,18 +8,45 @@ using System.Windows.Input;
 
 namespace BarNone.DataLift.UI.ViewModels
 {
+    /// <summary>
+    /// View Model for the Login Screen
+    /// </summary>
     public class LoginScreenVM : ViewModelBase
     {
         #region Types
+        /// <summary>
+        /// The login screen state enumeration
+        /// </summary>
         private enum LoginStates
         {
-            NoShow = 0, Spinner = 1, BadLogin = 2, GoodLogin = 3
+            /// <summary>
+            /// The enumeration state representing no addition draws are required
+            /// </summary>
+            NoShow = 0,
+            /// <summary>
+            /// The enumeration state representing a request has been made and is processing 
+            /// </summary>
+            Spinner = 1,
+            /// <summary>
+            /// The enumeration state representing a request returned "Not successful"
+            /// </summary>
+            BadLogin = 2,
+            /// <summary>
+            /// The enumeration state representing a request returned "Successful"
+            /// </summary>
+            GoodLogin = 3
         }
 
         #endregion
 
         #region Bound Properties
-        internal static string _username = "";
+        /// <summary>
+        /// Field representation for the <see cref="Username"/> bindable property
+        /// </summary>
+        private static string _username = "";
+        /// <summary>
+        /// Binding property for the user inputted username
+        /// </summary>
         public string Username
         {
             get => _username;
@@ -33,8 +60,13 @@ namespace BarNone.DataLift.UI.ViewModels
             }
         }
 
-
+        /// <summary>
+        /// Field representation for the <see cref="Password"/> bindable property
+        /// </summary>
         private SecureString _password = new SecureString();
+        /// <summary>
+        /// Binding property for the user inputted password
+        /// </summary>
         public SecureString Password
         {
             get => _password;
@@ -47,9 +79,14 @@ namespace BarNone.DataLift.UI.ViewModels
                 }
             }
         }
-        
+
+        /// <summary>
+        /// Field representation for the <see cref="LoginState"/> bindable property
+        /// </summary>
         private LoginStates _loginState;
-        
+        /// <summary>
+        /// Holds the current state of the View FSM
+        /// </summary>
         private LoginStates LoginState
         {
             get => _loginState;
@@ -62,10 +99,17 @@ namespace BarNone.DataLift.UI.ViewModels
             }
         }
 
-        public bool IsProgressBarVisible {
+        /// <summary>
+        /// Oneway from source bindable property to determine if the login screen is processing a database request
+        /// </summary>
+        public bool IsProgressBarVisible
+        {
             get { return LoginState == LoginStates.Spinner; }
         }
-        
+
+        /// <summary>
+        /// Oneway from source bindable property to determine if the login screen is displaying that a bad login attempt was made
+        /// </summary>
         public bool IsBadLogin
         {
             get { return LoginState == LoginStates.BadLogin; }
@@ -74,8 +118,13 @@ namespace BarNone.DataLift.UI.ViewModels
         #endregion
 
         #region Commands
+        /// <summary>
+        /// Field representation for the <see cref="LoginCommand"/> bindable command
+        /// </summary>
         private RelayCommand _loginCommand;
-
+        /// <summary>
+        /// Command binding to produe a login request with entered credentials
+        /// </summary>
         public ICommand LoginCommand
         {
             get
@@ -88,23 +137,34 @@ namespace BarNone.DataLift.UI.ViewModels
             }
         }
 
-        private bool IsLoginStateUserModifiable()
-        {
-            return LoginState != LoginStates.Spinner; 
-        }
-
+        /// <summary>
+        /// Field representation for the <see cref="RegisterCommand"/> bindable command
+        /// </summary>
+        private RelayCommand _registerCommand;
+        /// <summary>
+        /// Command binding to produe a login request with entered credentials
+        /// </summary>
         public ICommand RegisterCommand
         {
+
             get
             {
-                return new RelayCommand(action => PageManager.SwitchPage(UIPages.RegistrationView), pred => IsLoginStateUserModifiable());
+                if (_registerCommand == null)
+                {
+                    _registerCommand = new RelayCommand(action => PageManager.SwitchPage(UIPages.RegistrationView), pred => IsLoginStateUserModifiable());
+                }
+                return _registerCommand;
             }
         }
-        
+
+        /// <summary>
+        /// Task to request the auth for the inputted user credentials
+        /// </summary>
+        /// <returns>Void task</returns>
         private async Task LoginAsync()
         {
             LoginState = LoginStates.Spinner;
-            if (await CanLogin())
+            if (await WasLoginSuccessful())
             {
                 //Login server calls here to get a valid token and shift to data recorder or notify bad user pass combo
                 PageManager.SwitchPage(UIPages.ControlHolderView);
@@ -116,21 +176,38 @@ namespace BarNone.DataLift.UI.ViewModels
             }
         }
 
-        private async Task<bool> CanLogin()
+        /// <summary>
+        /// Task for determining if a login is successful
+        /// </summary>
+        /// <returns>If the login succeeded</returns>
+        private async Task<bool> WasLoginSuccessful()
         {
             var r = await TokenManager.Authorize(Username, ConvertSecure(Password));
             return r.Authorized;
         }
 
+        /// <summary>
+        /// Internal method for determining if the user should be able to modify information on the Login Screen
+        /// </summary>
+        /// <returns>If the login screen can be modified by the user at the isntance of call</returns>
+        private bool IsLoginStateUserModifiable()
+        {
+            return LoginState != LoginStates.Spinner;
+        }
         #endregion
-        
+
         #region CleanUp
-        //Destructor to ensure the password is removed from the system
+        /// <summary>
+        /// Destructor for the login screen, this ensure the Password is disposed and handled properly 
+        /// </summary>
         ~LoginScreenVM()
         {
             Password.Dispose();
         }
 
+        /// <summary>
+        /// Method to reset the Login Screen FSM, called on screen load
+        /// </summary>
         internal override void Loaded()
         {
             Username = "";
