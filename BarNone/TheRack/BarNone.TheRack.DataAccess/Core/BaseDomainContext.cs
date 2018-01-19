8199633b2f275c3db1a5ffff548150dac571c9fd
Chris Stokes <christopher.stokes@snowjob.ca>
2018-01-18 23:09:48 -0500

    comments

--- BarNone/TheRack/BarNone.TheRack.DataAccess/Core/BaseDomainContext.cs
+++ BarNone/TheRack/BarNone.TheRack.DataAccess/Core/BaseDomainContext.cs
@@ -37,15 +37,38 @@ namespace BarNone.TheRack.DataAccess
         /// </summary>
         private static readonly string DATABASE = "BarNone";
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseDomainContext"/> class.
+        /// </summary>
         public BaseDomainContext() : base()
         {
         }
 
+        /// <summary>
+        /// <para>
+        /// Override this method to configure the database (and other options) to be used for this context.
+        /// This method is called for each instance of the context that is created.
+        /// </para>
+        /// <para>
+        /// In situations where an instance of <see cref="T:Microsoft.EntityFrameworkCore.DbContextOptions" /> may or may not have been passed
+        /// to the constructor, you can use <see cref="P:Microsoft.EntityFrameworkCore.DbContextOptionsBuilder.IsConfigured" /> to determine if
+        /// the options have already been set, and skip some or all of the logic in
+        /// <see cref="M:Microsoft.EntityFrameworkCore.DbContext.OnConfiguring(Microsoft.EntityFrameworkCore.DbContextOptionsBuilder)" />.
+        /// </para>
+        /// </summary>
+        /// <param name="optionsBuilder">A builder used to create or modify options for this context. Databases (and other extensions)
+        /// typically define extension methods on this object that allow you to configure the context.</param>
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql(Credentials);
         }
 
+        /// <summary>
+        /// Gets the credentials.
+        /// </summary>
+        /// <value>
+        /// The credentials.
+        /// </value>
         public static string Credentials
         {
             get
