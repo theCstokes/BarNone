1ef0f4bec52f6f6966da99596041972ed7c7a8b5
Riley McGee <mcgeer@mcmaster.ca>
2018-01-18 20:14:44 -0500

    Commented Common

--- BarNone/DataLift/BarNone.DataLift.Application/ViewModels/Common/CurrentLiftDataVM.cs
+++ BarNone/DataLift/BarNone.DataLift.Application/ViewModels/Common/CurrentLiftDataVM.cs
@@ -1,21 +1,24 @@
 ﻿using BarNone.Shared.DataTransfer;
-using BarNone.DataLift.UI.ViewModels;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using Newtonsoft.Json;
 
 namespace BarNone.DataLift.UI.ViewModels.Common
 {
+    /// <summary>
+    /// Shared View Model for all Views that require a refference to the current sessions recorded data
+    /// </summary>
     public class CurrentLiftDataVM : ViewModelBase
     {
         //If we need to get item modified ask me -Riley
+        /// <summary>
+        /// Field representation for the <see cref="CurrentRecordedData"/> bindable property list
+        /// </summary>
         private ObservableCollection<LiftDTO> _currentRecordedData = new ObservableCollection<LiftDTO>();
+        /// <summary>
+        /// Binding property for the currently recorded data. This data represents all recorded data from start recording to stop recorrding.
+        /// </summary>
         public ObservableCollection<LiftDTO> CurrentRecordedData
         {
             get => _currentRecordedData;
@@ -26,6 +29,9 @@ namespace BarNone.DataLift.UI.ViewModels.Common
             }
         }
 #if DEBUG
+        /// <summary>
+        /// Constructor for CurrentLiftDataVM which initializes test data into the system for UI validation
+        /// </summary>
         public CurrentLiftDataVM()
         {
             CurrentRecordedData.Add(JsonConvert.DeserializeObject<LiftDTO>(File.ReadAllText(@"C:\Users\Aamir\Documents\McMaster\Year_4\Capstone\barnone\BarNone\DataLift\BarNone.DataLift.Application\bin\Debug\SQUAT_2018_01_13.json")));
