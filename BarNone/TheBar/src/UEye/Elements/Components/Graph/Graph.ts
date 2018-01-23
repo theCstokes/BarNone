b1d624981bb561c23b80dd325261ebf3de2e1be5
vgulatee <vishesh_g@hotmail.com>
2018-01-23 13:48:38 -0500

    Graph Basic Wrapper Complete

--- BarNone/TheBar/src/UEye/Elements/Components/Graph/Graph.ts
+++ BarNone/TheBar/src/UEye/Elements/Components/Graph/Graph.ts
@@ -3,60 +3,127 @@
 // import { Chart } from "../../../../../node_modules/chart.js/src/chart";
 import { BaseComponent } from "UEye/Elements/Core/BaseComponent/BaseComponent";
 import Core from "UEye/Elements/Core/Core";
+/**Type Definition: for LineData to be drawn on canvas */
+type LineData = { 
+    /**x-coordinate on canvas*/
+    x: number, 
+      /**y-coordinate on canvas*/
+    y: number };
+
 
 const Chart = require("chartjs");
 console.log(Chart);
-
+/**Describes the properties for Chart and Graph related operations. Wrapper for API required from Chart.js*/
 export default class Graph extends BaseComponent {
+    /**Represents HTMLCanvasElement to draw graphs on*/
     private _canvas: HTMLCanvasElement;
+     /**Represents context to render on the canvas element (paintbrush)*/
     private _context: CanvasRenderingContext2D;
-
+     /**Represents type of graph being draw as a string value (eg. Bar for Bar graph and Line for Line graph)*/
+    private _type: string;
+    /**Represents array of linedata that plots the complete graph */
+    private _data: LineData[];
+     /**Represents the label along the X-Axis */
+    private _xAxisLabel: string;
+      /**Represents the label along the Y-Axis */
+    private _yAxisLabel: string;
+      /**Represents Title of Graph*/
+    private _title: string;
+      /** Constructor intializes and defines the Graph component as an HTMLElement tag named UEye-Graph as well as the context needed for plotting graphs. 
+     * @param parent - Represents properties of the current element as an HTMLElement.
+	 * * @returns Returns a Graph type with the referenced 2d context.   
+     * 
+     * */
     public constructor(parent: HTMLElement) {
         super(parent, "UEye-Graph");        
 
         this._canvas = Core.create("canvas", this.element, "Canvas") as HTMLCanvasElement;
         var c = this._canvas.getContext('2d');
+        this._title= "Graph";
+       
+        
         if (c !== null) { this._context = c; }
 
         // Chart(this._context);
 
-        var chart = new Chart(this._context, {
-            type: 'bar',
-            data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            // beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
     }
+    public get type(): string {
+		return this._type;
+    }
+    
+	public set type(value: string) {
+		this._type = value;
+		
+    }
+    public set xAxisLabel(value: string) {
+		this._xAxisLabel = value;
+    }
+    public get xAxisLabel (): string {
+		return this._xAxisLabel;
+    }
+    public set yAxisLabel(value: string) {
+		this._yAxisLabel = value;
+    }
+    public get yAxisLabel (): string {
+		return this._yAxisLabel;
+    }
+    public set title(value: string) {
+		this._title = value;
+    }
+    public get title (): string {
+		return this._title;
+    }
+    public set data(value: LineData[]) {
+		this._data = value;
+    }
+    public get data (): LineData[] {
+		return this._data;
+    }
+    public set draw (value:boolean) {
+       
+        if(value==true){
+            var chart = new Chart(this._context, {
+                type: 'line',
+                data: {
+                  //  labels: ["0", "15", "30", "45", "60", "75"],
+                    datasets: [{
+                        label: this._title,
+                        fill: false,
+                         data: this._data,
+    
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            },
+                          scaleLabel: {
+                            display: true,
+                            labelString: this._xAxisLabel
+                           
+                          }
+                        }],
+                        xAxes: [{
+                            type: 'linear',
+                            ticks: {
+                                beginAtZero: true,
+                                min: 0,
+                                max: 100,
+                                stepSize: 15
+                            },
+                            scaleLabel: {
+                              display: true,
+                              labelString: this._yAxisLabel
+                            }
+                          }],
+                      }  
+                }
+            });
+        }
+        }
+    
 
     public onEnabledChange(): void {
         throw new Error("Method not implemented.");
