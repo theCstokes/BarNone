32b45dc34334d0d71aebba7596f49b15716f8e4c
Aamir Mansoor <mansoa4@mcmaster.ca>
2018-03-08 01:23:26 -0500

    Added custom implementation for Users so that the display looks nice. TODO link the selected portion of the list, work on search.  I need to think of a good way to do search

--- BarNone/DataLift/BarNone.DataLift.Application/ViewModels/SaveLiftVM.cs
+++ BarNone/DataLift/BarNone.DataLift.Application/ViewModels/SaveLiftVM.cs
@@ -2,11 +2,14 @@
 using BarNone.DataLift.UI.Commands;
 using BarNone.DataLift.UI.ViewModels.Common;
 using BarNone.Shared.DataConverters;
+using BarNone.Shared.DataTransfer;
 using BarNone.Shared.DataTransfer.Flex;
 using BarNone.Shared.DomainModel;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -20,6 +23,98 @@ namespace BarNone.DataLift.UI.ViewModels
         #region Common Data
         public CurrentLiftDataVM CurrentLiftData = CurrentLiftDataVMSingleton.GetInstance();
         #endregion
+
+        #region TEMP DATA FOR TESTING
+        private ObservableCollection<SharableUser> _users = new ObservableCollection<SharableUser> {
+            new SharableUser()
+            {
+                Name = "Chris Stokes",
+                UserName = "TheAryan"
+            },
+            new SharableUser()
+            {
+                Name = "Aamir Mansoor",
+                UserName = "Csharp70"
+            },
+            new SharableUser()
+            {
+                Name = "Jon Brown",
+                UserName = "NotActuallyBrown"
+            },
+            new SharableUser()
+            {
+                Name = "Riley McGee",
+                UserName = "Fuggles"
+            },
+            new SharableUser(){
+                Name = "Vishesh Gulatee",
+                UserName = "ActuallyBrown"
+            }
+        };
+        public ObservableCollection<SharableUser> Users
+        {
+            get
+            {
+                return _users;
+            }
+            set
+            {
+                _users = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Users"));
+            }
+        }
+        #endregion
+
+        #region Custom User Implementation
+        public class SharableUser
+        {
+            public string Name
+            {
+                get; set;
+            }
+            public string UserName
+            {
+                get; set;
+            }
+
+            public string Code
+            {
+                get
+                {
+                    return UserName[0].ToString();
+                }
+
+                private set { }
+            }
+
+            public bool IsSeletcted
+            {
+                get; set;
+            }
+
+            public SharableUser()
+            {
+                IsSeletcted = false;
+            }
+        }
+        #endregion
+
+        #region UserSharePropeties
+        private ObservableCollection<User> _selectedUsers;
+        public ObservableCollection<User> SelectedUsers
+        {
+            get
+            {
+                return _selectedUsers;
+            }
+            set
+            {
+                _selectedUsers = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("SelectedUsers"));
+            }
+        }
+        #endregion
+
         #region Public Commands
         /// <summary>
         /// Field representation for the <see cref="StartRecording"/> bindable command
@@ -85,6 +180,16 @@ namespace BarNone.DataLift.UI.ViewModels
 
             Console.WriteLine("Send Lift functionality to be implemented.");
         }
+        #endregion
+
+        #region Loaded and Closed
+
+        // To be uncommented when I actually have the DB.
+        //async internal override void Loaded()
+        //{
+        //    List<UserDTO> allUserDTOs = await DataManager.Users.GetAll();
+        //    List<User> allUserDM = allUserDTOs.Select(x => Converters.NewConvertion().User.CreateDataModel(x)).ToList();
+        //}
         #endregion
     }
 }
